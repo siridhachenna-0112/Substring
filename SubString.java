e47e5010b0bb0941c1207ec3636a1f5a737451fe
siridhachenna-0112 <chennasiridha5002@gmail.com>
2025-01-25 14:39:40 +0530

    Add files via upload

---
+++ SubString.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class SubString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.println(a.substring(0, 6));
+    }
+}
